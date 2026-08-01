2fcdae6bf1897274e945997b63d3c55dae15232c
satishbabu7777 <satishbabu7777@gmail.com>
2026-08-01 17:32:54 +0530

    updated end pointe response

--- booking/src/main/java/com/ecommerce/booking/controller/BookingController.java
+++ booking/src/main/java/com/ecommerce/booking/controller/BookingController.java
@@ -29,7 +29,7 @@ public class BookingController {
 
     @GetMapping("/{name}")
     public String testApi(@PathVariable String name) {
-        return name + ":  Thank you Jeeus for teaching me to prepare CICD pipeline for spring boot application with helm chart and prometheus integration";
+        return name + ":  Thank you Jeeeus for teaching me to prepare CICD pipeline for spring boot application with helm chart and prometheus integration";
     }
 
 }
